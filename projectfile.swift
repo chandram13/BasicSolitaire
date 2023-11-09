46e2daa6c09cfdec13a51f97ee109fa37b094b3f
Marvish Subhas Chandra <mschandr@uci.edu>
2023-11-09 13:41:43 -0800

    Add files via upload

--- projectfile.swift
+++ projectfile.swift
@@ -8,7 +8,21 @@
 import Foundation
 
 
+
 pileOne = "Heards"
 pileTwo = "Spades"
 pileThree = "Diamonds"
 pileFour = "Clubs"
+
+
+// create seven stacks, indicate number of cards
+shuffledCards = 24
+firtStack = 1
+secondStack = 2
+thirdStack = 3
+fourthStack = 4
+fifthStack = 5
+sixthStack = 6
+seventhStack = 7
+
+constructDeck = [1: "A", 2: "2", 3:"3", 4: "4", 5:"5", 6:"6", 7:"7", 8:"8", 9: "9", 10: "10", 11: "J", 12:"J",13:"Q",14:"K"]
