c8d958d74c308a477074d0fb0dd2f245daa65a0e
Marvish Subhas Chandra <mschandr@uci.edu>
2023-11-09 11:03:20 -0800

    Add files via upload

---
+++ projectfile.swift
@@ -0,0 +1,14 @@
+//
+//  projectfile.swift
+//  BasicSolitaire
+//
+//  Created by Marvish Chandra on 11/9/23.
+//
+
+import Foundation
+
+
+pileOne = "Heards"
+pileTwo = "Spades"
+pileThree = "Diamonds"
+pileFour = "Clubs"
