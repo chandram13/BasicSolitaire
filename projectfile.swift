38b1b8b7d9826bc2c63133a90661c5a3519624c7
Marvish Subhas Chandra <mschandr@uci.edu>
2023-11-09 13:51:02 -0800

    Add files via upload

--- projectfile.swift
+++ projectfile.swift
@@ -7,7 +7,11 @@
 
 import Foundation
 
+import random
+
+
 class Solitaire:
+    constructDeck = {1: "A", 2: "2", 3:"3", 4: "4", 5:"5", 6:"6", 7:"7", 8:"8", 9: "9", 10: "10", 11: "J", 12:"J",13:"Q",14:"K"}
 
 pileOne = "Heards"
 pileTwo = "Spades"
@@ -25,4 +29,4 @@ fifthStack = 5
 sixthStack = 6
 seventhStack = 7
 
-constructDeck = {1: "A", 2: "2", 3:"3", 4: "4", 5:"5", 6:"6", 7:"7", 8:"8", 9: "9", 10: "10", 11: "J", 12:"J",13:"Q",14:"K"}
+
